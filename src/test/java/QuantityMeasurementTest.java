1e65a5982e05b63280f4160b98a30c449869e52b
nnalin04 <nnalin04@gmail.com>
2020-09-04 21:48:04 +0530

    Added test and code to perform test for equality for Inch

--- src/test/java/QuantityMeasurementTest.java
+++ src/test/java/QuantityMeasurementTest.java
@@ -25,10 +25,56 @@ public class QuantityMeasurementTest {
     }
 
     @Test
+    public void givenTwoDifferentType_WhenCompared_ShouldReturnEqual() {
+        Feet quantity1 = new Feet(0.0);
+        Inch quantity2 = new Inch(0.0);
+        Assert.assertNotEquals(quantity1, quantity2);
+    }
+
+    @Test
     public void givenNullMeasurement_WhenCompared_ShouldReturnEqual() throws NullPointerException {
         try {
             Feet quantity1 = new Feet(null);
             Feet quantity2 = new Feet(null);
+            Assert.assertNotEquals(quantity1, quantity2);
+        }catch (NullPointerException e){
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void givenSimilarMeasurementInInch_WhenCompared_ShouldReturnEqual() {
+        Inch quantity1 = new Inch(0.0);
+        Inch quantity2 = new Inch(0.0);
+        Assert.assertEquals(quantity1, quantity2);
+    }
+
+    @Test
+    public void givenSimilarInchObject_WhenCompared_ShouldReturnEqual() {
+        Inch quantity1 = new Inch(0.0);
+        Inch quantity2 = quantity1;
+        Assert.assertEquals(quantity1, quantity2);
+    }
+
+    @Test
+    public void givenNonSimilarMeasurementInInch_WhenCompared_ShouldReturnEqual() {
+        Inch quantity1 = new Inch(0.0);
+        Inch quantity2 = new Inch(1.0);
+        Assert.assertNotEquals(quantity1, quantity2);
+    }
+
+    @Test
+    public void givenTwoDifferentTypeObject_WhenCompared_ShouldReturnEqual() {
+        Feet quantity1 = new Feet(0.0);
+        Inch quantity2 = new Inch(0.0);
+        Assert.assertNotEquals(quantity1, quantity2);
+    }
+
+    @Test
+    public void givenNullMeasurementInInch_WhenCompared_ShouldReturnEqual() throws NullPointerException {
+        try {
+            Inch quantity1 = new Inch(null);
+            Inch quantity2 = new Inch(null);
             Assert.assertNotEquals(quantity1, quantity2);
         }catch (NullPointerException e){
             e.printStackTrace();
